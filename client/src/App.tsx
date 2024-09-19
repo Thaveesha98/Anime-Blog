8a823821ccb2ba6a18bcf4ddc7d3342399bf759c
unknown <thaveeshayanith98@gmail.com>
2024-09-19 18:02:54 +0530

    landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SignUp from "./pages/signup/SignUp";
 import Login from "./pages/login/Login";
+import LandingPage from "./pages/landing-page/LandingPage";
 
 function App() {
   return (
     <div className="App">
       {/* <SignUp /> */}
-      <Login />
+      {/* <Login /> */}
+      <LandingPage />
     </div>
   );
 }
--- client/src/pages/landing-page/LandingPage.tsx
+++ client/src/pages/landing-page/LandingPage.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import CustomInput from "../../common/CustomInput";
 
 type Props = {};
 
 const LandingPage = (props: Props) => {
-  return <div>LandingPage</div>;
+  return (
+    <>
+      <img
+        src="assets/Home.png"
+        alt="Anime Land"
+        className="w-[100vw] h-[100dvh] absolute -z-10"
+      />
+      <div className="absolute w-[35em] space-y-10 top-[40%] left-[5%] -translate-x-[5%] -translate-y-[40%]">
+        <h1 className="text-white text-[4.5em] leading-[70px]">
+          Welcome to Gates
+        </h1>
+        <p className="text-white text-[1.5em] leading-8">
+          Get the latest news on your favorite manga, anime and manhwa around
+          the world!
+        </p>
+        <div className="">
+          <CustomInput label={"Subscribe"} placeholder="Enter Your Email" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LandingPage;
